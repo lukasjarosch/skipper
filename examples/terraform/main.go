ebee7d93167b0697491712fc32cfbcfa03669f24
Lukas Jarosch <lukas.jarosch@mail.com>
2022-07-30 13:13:14 +0200

    feat: implement new templater; cleanup

--- examples/terraform/main.go
+++ examples/terraform/main.go
@@ -2,159 +2,87 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"io/fs"
 	"log"
 	"path"
-	"path/filepath"
-	"strings"
-	"text/template"
 
-	"github.com/Masterminds/sprig/v3"
 	"github.com/spf13/afero"
-	"gopkg.in/yaml.v3"
 
 	"github.com/lukasjarosch/skipper"
 )
 
 var (
-	dataPath     string
-	target       string
-	templatePath string
-	outputPath   string
+	inventoryPath string
+	templatePath  string
+	outputPath    string
+	target        string
+
+	targetPath string
+	classPath  string
 
 	fileSystem = afero.NewOsFs()
 )
 
 func init() {
-	flag.StringVar(&dataPath, "data", "inventory", "path to the data folder")
-	flag.StringVar(&target, "target", "", "name of the target to use")
+	flag.StringVar(&inventoryPath, "data", "inventory", "path to the inventory folder")
 	flag.StringVar(&templatePath, "templates", "templates", "path to the templates folder")
 	flag.StringVar(&outputPath, "output", "output", "template output path")
+	flag.StringVar(&target, "target", "", "name of the target to use")
 	flag.Parse()
+
+	targetPath = path.Join(inventoryPath, "targets")
+	classPath = path.Join(inventoryPath, "classes")
 }
 
 func main() {
-	if dataPath == "" {
-		log.Fatalln("data path cannot be empty")
-	}
-	if templatePath == "" {
-		log.Fatalln("templates path cannot be empty")
-	}
 	if target == "" {
 		log.Fatalln("target cannot be empty")
 	}
-	if outputPath == "" {
-		log.Fatalln("outputPath cannot be empty")
-	}
-
-	log.Printf("inventory path set to '%s'", dataPath)
+	log.Printf("inventory path set to '%s'", inventoryPath)
 	log.Printf("template path set to '%s'", templatePath)
 	log.Printf("compiled path set to '%s'", outputPath)
 	log.Printf("desired target is '%s'", target)
 
-	// load inventory ----------------------------------------------------------------------------------
-
+	// initialiye and load inventory ----------------------------------------------------------------------------------
 	inventory, err := skipper.NewInventory(afero.NewOsFs())
 	if err != nil {
-		// TODO: handle error instead of panicking
 		panic(err)
 	}
-
-	// TODO: make configurable
-	targetPath := path.Join(dataPath, "targets")
-	classPath := path.Join(dataPath, "classes")
-	log.Printf("target path: '%s'", targetPath)
-	log.Printf("classes path: '%s'", classPath)
 
 	err = inventory.Load(classPath, targetPath)
 	if err != nil {
-		// TODO: handle error instead of panicking
 		panic(err)
 	}
 
-	// discover templates ----------------------------------------------------------------------------------
-
-	// iterate over templatePath, where we set the filesystem root to templatePath
-	// this allows us to only get relative paths to the template file
-	var templateFiles []string
-	err = afero.Walk(afero.NewBasePathFs(fileSystem, templatePath), "", func(path string, info fs.FileInfo, err error) error {
-		if info.IsDir() {
-			return nil
-		}
-		templateFiles = append(templateFiles, path)
-		return nil
-	})
+	// discover, load and parse the templates ----------------------------------------------------------------------------------
+	templateOutputPath := path.Join(outputPath, target)
+	templater, err := skipper.NewTemplater(fileSystem, templatePath, templateOutputPath)
 	if err != nil {
-		// TODO: handle error instead of panicking
 		panic(err)
+	}
+
+	for _, template := range templater.Files {
+		log.Println("discovered template", template.Path)
 	}
 
 	// render inventory data based on target ----------------------------------------------------------------------------------
-
 	data, err := inventory.Data(target)
 	if err != nil {
 		panic(err)
 	}
 
-	// render output  ----------------------------------------------------------------------------------
-
-	// render templates
-	for _, templateFile := range templateFiles {
-		templateFilePath := path.Join(templatePath, templateFile)
-
-		tpl := template.New(filepath.Base(templateFile)).Funcs(sprig.TxtFuncMap()).Funcs(map[string]any{
-			"tfStringArray": func(input []interface{}) string {
-				var s []string
-				for _, v := range input {
-					s = append(s, "\""+fmt.Sprintf(v.(string))+"\"")
-				}
-				return strings.Join(s, ", ")
-			},
-		})
-
-		tplContent, err := afero.ReadFile(fileSystem, templateFilePath)
-		if err != nil {
-			// TODO: handle error instead of panicking
-			panic(err)
-		}
-
-		tpl, err = tpl.Parse(string(tplContent))
-		if err != nil {
-			// TODO: handle error instead of panicking
-			panic(err)
-		}
-
-		// create the file output path: <output>/<target>/<template>
-		fileOutputPath := path.Join(outputPath, target, templateFile)
-
-		err = fileSystem.MkdirAll(filepath.Dir(fileOutputPath), 0755)
-		if err != nil {
-			// TODO: handle error instead of panicking
-			panic(err)
-		}
-
-		outFile, err := fileSystem.Create(fileOutputPath)
-		if err != nil {
-			// TODO: handle error instead of panicking
-			panic(err)
-		}
-
-		templateData := struct {
-			Inventory skipper.Data
-		}{
-			Inventory: data,
-		}
-
-		err = tpl.Execute(outFile, templateData)
-		if err != nil {
-			// TODO: handle error instead of panicking
-			panic(err)
-		}
+	templateData := struct {
+		Inventory skipper.Data
+	}{
+		Inventory: data,
 	}
 
-	s, _ := yaml.Marshal(data)
-	fmt.Println(string(s))
-
-	log.Printf("%+v", data)
+	// execute templates  ----------------------------------------------------------------------------------
+	for _, template := range templater.Files {
+		err := templater.Execute(template, templateData)
+		if err != nil {
+			panic(err)
+		}
+		log.Printf("executed template '%s' into: %s'", template.Path, path.Join(templateOutputPath, template.Path))
+	}
+	// Alternatively `templater.ExecuteAll(template, templateData)` can be used
 }
