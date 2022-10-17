9d30842cb931f260d179118a8fb6baee51ab2a60
Lukas Jarosch <lukas.jarosch@mail.com>
2022-10-17 15:26:10 +0200

    feat: add set-policy to keyvault

--- examples/keyvault/templates/apply.sh
+++ examples/keyvault/templates/apply.sh
@@ -9,3 +9,7 @@ az keyvault create \
   --resource-group {{ .Inventory.keyvault.resource_group }} \
   --name {{ .Inventory.keyvault.name }}
 
+az keyvault set-policy \
+  -n {{ .Inventory.keyvault.name }} \
+  --key-permissions backup create decrypt delete encrypt get getrotationpolicy import list purge recover release restore rotate setrotationpolicy sign unwrapKey update verify wrapKey \
+  --object-id $(az ad signed-in-user show | jq -r '.id')
