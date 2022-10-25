abe3e56aa6daa532e379c4c42efb07726a2bded0
Lukas Jarosch <lukas.jarosch@mail.com>
2022-10-25 13:07:08 +0200

    feat: test error annotations

--- .github/scripts/examples_test.sh
+++ .github/scripts/examples_test.sh
@@ -3,7 +3,6 @@
 examplesDir="$1"
 testdataDir="testdata"
 testFile="main_test.go"
-exitCode=0
 
 for dir in ${examplesDir}/*; do
   echo "Entering $dir"
@@ -11,28 +10,23 @@ for dir in ${examplesDir}/*; do
 
   # examples usually only have a 'main.go', hence the test 'main_test.go' must exist
   if [[ ! -f $testFile ]]; then
-    echo "Error: missing test file '$testFile'" && $(exit $exitCode) # trigger github error detection
-    exitCode=1
+    echo "::error:: Missing test file '$testFile'"
     continue
   fi
 
   # testdata directory must exist
   if [[ ! -d $testdataDir ]]; then
-    echo "Error: missing '$testdataDir' directory" &&  $(exit $exitCode) # trigger github error detection
-    exitCode=1
+    echo "::error:: Missing '$testdataDir' directory"
     continue
   fi
 
   # at least one golden file fixture needs to be present
   fileCount=$(ls $testdataDir | wc -l)
   if [[ ! $fileCount -gt 0 ]]; then
-    echo "Error: no golden files in '$testdataDir'" && $(exit $exitCode) # trigger github error detection
-    exitCode=1
+    echo "::error:: no golden files in '$testdataDir'"
     continue
   fi
 
   # TODO: execute test
 
 done
-
-exit $exitCode
