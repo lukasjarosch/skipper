b2ff896bad853935a4eb2792dbab4ef0453639e9
Lukas Jarosch <lukas.jarosch@mail.com>
2023-10-02 22:28:02 +0200

    feat: add `data.Patch` option to patch existing containers; add `MustGetValue` for convenience

--- data/inventory.go
+++ data/inventory.go
@@ -9,6 +9,7 @@ var (
 	RootNamespace = make(Path, 0)
 
 	ErrContainerExists = fmt.Errorf("container already exists")
+	ErrNilContainer    = fmt.Errorf("container is nil")
 )
 
 // Container defines an interface of a container holding values used by the [Inventory].
@@ -22,7 +23,7 @@ type Container interface {
 type Inventory struct {
 	// namespaces is a map of namespace strings to a map of container names -> [Container]
 	namespaces map[string]map[string]Container
-	// pathRegistry is a map of [Path]s strings to a [Scope] in which the value is located.
+	// pathRegistry is a map of [Path]s strings to a [ValueScope] in which the value is located.
 	pathRegistry map[string]ValueScope
 }
 
@@ -39,12 +40,19 @@ func NewInventory() (*Inventory, error) {
 
 type RegisterOpts struct {
 	ReplaceExisting bool
+	Patch           bool
 }
 type RegisterOption func(*RegisterOpts)
 
 func ReplaceContainer() RegisterOption {
 	return func(ro *RegisterOpts) {
 		ro.ReplaceExisting = true
+	}
+}
+
+func Patch() RegisterOption {
+	return func(ro *RegisterOpts) {
+		ro.Patch = true
 	}
 }
 
@@ -59,16 +67,20 @@ func (inv *Inventory) UnregisterContainer(namespace Path, containerName string) 
 }
 
 func (inv *Inventory) RegisterContainer(namespace Path, container Container, options ...RegisterOption) error {
+	if container == nil {
+		return ErrNilContainer
+	}
+	if len(container.Name()) == 0 {
+		return ErrEmptyContainerName
+	}
+
 	// handle options
 	opts := RegisterOpts{
 		ReplaceExisting: false,
+		Patch:           false,
 	}
 	for _, opt := range options {
 		opt(&opts)
-	}
-
-	if len(container.Name()) == 0 {
-		return ErrEmptyContainerName
 	}
 
 	// just to be explicit about it, if the namespace is of length 0, it is always the root namespace
@@ -85,10 +97,13 @@ func (inv *Inventory) RegisterContainer(namespace Path, container Container, opt
 	// If the container already exists unregister it if the ReplaceExisting option is set,
 	// otherwise fail (default)
 	if _, exists := inv.namespaces[namespaceString][container.Name()]; exists {
-		if !opts.ReplaceExisting {
-			return fmt.Errorf("%s: %w", container.Name(), ErrContainerExists)
+		if !opts.ReplaceExisting && !opts.Patch {
+			return fmt.Errorf("%s.%s: %w", namespace, container.Name(), ErrContainerExists)
 		}
-		inv.UnregisterContainer(namespace, container.Name())
+		// if replace mode is enabled, remove the existing container and it's data alltogether
+		if opts.ReplaceExisting {
+			inv.UnregisterContainer(namespace, container.Name())
+		}
 	}
 
 	// Value paths must stay unique within the [Inventory]
@@ -97,13 +112,20 @@ func (inv *Inventory) RegisterContainer(namespace Path, container Container, opt
 	{
 		for _, path := range container.AllPaths() {
 			// we're only interested in the absolute paths (namespace + path within container)
-			checkPath := namespace.AppendPath(path)
+			absPath := namespace.AppendPath(path)
 
-			if scope, exists := inv.pathRegistry[checkPath.String()]; exists {
-				return fmt.Errorf("path is already registered in namespace '%s' by container '%s': %s", scope.Namespace, scope.Container.Name(), checkPath)
+			if scope, exists := inv.pathRegistry[absPath.String()]; exists {
+				if !opts.Patch {
+					return fmt.Errorf("path is already registered in namespace '%s' by container '%s': %s", scope.Namespace, scope.Container.Name(), absPath)
+				}
+
+				// the path exists, but the patch option is set
+				// in this case we will continue and allow the scope to be changed to point to the patch container.
+				// WARN: the patch container is not in the namespace map, is this an issue? (One cannot directly fetch that container easily)
+				// TODO: preserve original scope to keep access to the original data
 			}
 
-			inv.pathRegistry[checkPath.String()] = ValueScope{
+			inv.pathRegistry[absPath.String()] = ValueScope{
 				Namespace:     namespace,
 				ContainerPath: path,
 				Container:     container,
@@ -114,6 +136,14 @@ func (inv *Inventory) RegisterContainer(namespace Path, container Container, opt
 	inv.namespaces[namespace.String()][container.Name()] = container
 
 	return nil
+}
+
+func (inv *Inventory) MustGetValue(path Path) Value {
+	val, err := inv.GetValue(path)
+	if err != nil {
+		panic(err)
+	}
+	return val
 }
 
 func (inv *Inventory) GetValue(path Path) (Value, error) {
