1726bec89cfa0c378bd895a1cf16264281b4e308
Lukas Jarosch <lukas.jarosch@mail.com>
2023-10-02 14:29:54 +0200

    feat: inventory path registry; ensure paths only exist once

--- data/inventory.go
+++ data/inventory.go
@@ -1,6 +1,8 @@
 package data
 
-import "fmt"
+import (
+	"fmt"
+)
 
 var (
 	// RootNamespace is defined by an empty path
@@ -10,13 +12,26 @@ var (
 	ErrEmptyContainerName = fmt.Errorf("container name empty")
 )
 
+type Scope struct {
+	Namespace Path
+	Container *Container
+}
+
 type Inventory struct {
+	// namespaces is a map of namespace strings to a map of container names -> [Container]
 	namespaces map[string]map[string]*Container
+	// pathRegistry is a map of [Path]s strings to a bool (true) used to quickly check the existence of a path
+	pathRegistry map[string]Scope
 }
 
 func NewInventory() (*Inventory, error) {
+	// create namespace registry and always register the root namespace
+	ns := make(map[string]map[string]*Container)
+	ns[RootNamespace.String()] = make(map[string]*Container)
+
 	return &Inventory{
-		namespaces: make(map[string]map[string]*Container, 0),
+		namespaces:   ns,
+		pathRegistry: make(map[string]Scope),
 	}, nil
 }
 
@@ -39,6 +54,25 @@ func (inv *Inventory) RegisterContainer(namespace Path, container *Container) er
 	// existing containers are not overwritten
 	if _, exists := inv.namespaces[namespaceString][container.Name]; exists {
 		return fmt.Errorf("%s: %w", container.Name, ErrContainerExists)
+	}
+
+	// Value paths must stay unique within the [Inventory]
+	// If one requests the value at 'foo.bar.baz', it cannot resolve to two different values.
+	// For this we need to build up a path registry which allows us to quickly check for existing paths.
+	{
+		for _, path := range container.AllPaths() {
+			// we're only interested in the absolute paths (namespace + path within container)
+			checkPath := namespace.AppendPath(path)
+
+			if scope, exists := inv.pathRegistry[checkPath.String()]; exists {
+				return fmt.Errorf("path is already registered in namespace '%s' by container '%s': %s", scope.Namespace, scope.Container.Name, checkPath)
+			}
+
+			inv.pathRegistry[checkPath.String()] = Scope{
+				Namespace: namespace,
+				Container: container,
+			}
+		}
 	}
 
 	inv.namespaces[namespace.String()][container.Name] = container
