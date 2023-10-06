2f708ba5d03890feb55c3a6af39cfcbd8ca756bf
Lukas Jarosch <lukas.jarosch@mail.com>
2023-10-07 01:17:05 +0200

    docs: add some basic docstrings

--- data/value.go
+++ data/value.go
@@ -5,18 +5,22 @@ import (
 	"time"
 )
 
+// Value represents a generic value that originates fom [Data]
 type Value struct {
 	Raw interface{}
 }
 
+// NewValue creates a new Value instance with the given raw value.
 func NewValue(raw interface{}) Value {
 	return Value{Raw: raw}
 }
 
+// String returns a string representation of the value.
 func (val Value) String() string {
 	return fmt.Sprint(val.Raw)
 }
 
+// Map attempts to convert the value to a Map. Returns an error if conversion is not possible.
 func (val Value) Map() (Map, error) {
 	m, ok := val.Raw.(Map)
 	if !ok {
@@ -25,6 +29,7 @@ func (val Value) Map() (Map, error) {
 	return m, nil
 }
 
+// Duration attempts to convert the value to a time.Duration. Returns an error if conversion is not possible.
 func (val Value) Duration() (time.Duration, error) {
 	dur, err := time.ParseDuration(val.String())
 	if err != nil {
@@ -33,6 +38,7 @@ func (val Value) Duration() (time.Duration, error) {
 	return dur, nil
 }
 
+// Int attempts to convert the value to an int. Returns an error if conversion is not possible.
 func (val Value) Int() (int, error) {
 	i, ok := val.Raw.(int)
 	if !ok {
@@ -41,6 +47,7 @@ func (val Value) Int() (int, error) {
 	return i, nil
 }
 
+// Int32 attempts to convert the value to an int32. Returns an error if conversion is not possible.
 func (val Value) Int32() (int32, error) {
 	i, ok := val.Raw.(int32)
 	if !ok {
@@ -49,6 +56,7 @@ func (val Value) Int32() (int32, error) {
 	return i, nil
 }
 
+// Int64 attempts to convert the value to an int64. Returns an error if conversion is not possible.
 func (val Value) Int64() (int64, error) {
 	i, ok := val.Raw.(int64)
 	if !ok {
