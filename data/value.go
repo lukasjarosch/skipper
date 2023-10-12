89d5b4a113bc7a666b6141149f5e7b150729c91c
Lukas Jarosch <lukas.jarosch@mail.com>
2023-10-12 22:35:26 +0200

    feat: remove inventory; this needs replacement in the main package

--- data/value.go
+++ data/value.go
@@ -64,31 +64,3 @@ func (val Value) Int64() (int64, error) {
 	}
 	return i, nil
 }
-
-// InventoryValue represents any value within the [Inventory]
-// It has a [ValueScope] which determines the scope in which the value is defined.
-type InventoryValue struct {
-	Value
-	Scope ValueScope
-}
-
-// ValueScope defines a scope in which a value is valid / defined.
-// It can also be used to retrieve the value again.
-type ValueScope struct {
-	// Namespace in which the value resides
-	Namespace Path
-	// The path within the container which points to the value
-	ContainerPath Path
-	// The actual container
-	Container Container
-}
-
-// AbsolutePath returns the absolute path to this value.
-// This is the namespace + ContainerPath.
-// The AbsolutePath can be used to retrieve the value from the [Inventory]
-//
-// Note that the absolute path does not work with a scoped Inventory, only with the root Inventory.
-// If you want to use it in a scoped inventory you need to strip the prefix yourself.
-func (scope ValueScope) AbsolutePath() Path {
-	return scope.Namespace.AppendPath(scope.ContainerPath)
-}
