72810d36ad5c1bf378d9ef858e18b458f2b5d70b
Lukas Jarosch <lukas.jarosch@mail.com>
2023-10-12 22:58:34 +0200

    test: add value tests

--- data/value_test.go
+++ data/value_test.go
@@ -1,3 +1,103 @@
 package data_test
 
-// TODO: ensure 'Raw' cannot be a function
+import (
+	"testing"
+	"time"
+
+	. "github.com/lukasjarosch/skipper/data"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValueString(t *testing.T) {
+	testCases := []struct {
+		name     string
+		value    Value
+		expected string
+	}{
+		{"IntegerValueToString", NewValue(42), "42"},
+		{"StringValueToString", NewValue("hello"), "hello"},
+		{"NilValueToString", NewValue(nil), "<nil>"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := tc.value.String()
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestValueMap(t *testing.T) {
+	testCases := []struct {
+		name     string
+		value    Value
+		expected Map
+		err      bool
+	}{
+		{"ValidMapValue", NewValue(Map{"key": "value"}), Map{"key": "value"}, false},
+		{"InvalidMapValue", NewValue(42), nil, true},
+		{"NilValue", NewValue(nil), nil, true},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := tc.value.Map()
+			if tc.err {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expected, result)
+			}
+		})
+	}
+}
+
+func TestValueDuration(t *testing.T) {
+	testCases := []struct {
+		name     string
+		value    Value
+		expected time.Duration
+		err      bool
+	}{
+		{"ValidDurationString", NewValue("1s"), time.Second, false},
+		{"InvalidDurationString", NewValue("invalid_duration"), 0, true},
+		{"NilValue", NewValue(nil), 0, true},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := tc.value.Duration()
+			if tc.err {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expected, result)
+			}
+		})
+	}
+}
+
+func TestValueInt(t *testing.T) {
+	testCases := []struct {
+		name     string
+		value    Value
+		expected int
+		err      bool
+	}{
+		{"ValidIntValue", NewValue(42), 42, false},
+		{"InvalidIntValue", NewValue("invalid_int"), 0, true},
+		{"NilValue", NewValue(nil), 0, true},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := tc.value.Int()
+			if tc.err {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expected, result)
+			}
+		})
+	}
+}
