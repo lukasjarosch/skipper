7a277818d2a5ffed0d52559e254c0070e4ff90e9
Lukas Jarosch <lukas.jarosch@mail.com>
2024-02-15 16:14:25 +0100

    test: more Reference tests
    Signed-off-by: Lukas Jarosch <lukas.jarosch@mail.com>

--- reference_test.go
+++ reference_test.go
@@ -10,6 +10,10 @@ import (
 	"github.com/lukasjarosch/skipper/data"
 )
 
+// --------------------------------------------------
+// Class Reference Tests
+// --------------------------------------------------
+
 var expectedReferencesSimple = []Reference{
 	{
 		Path:       data.NewPath("simple.departments.engineering.manager"),
@@ -36,11 +40,7 @@ func TestParseReferencesSimple(t *testing.T) {
 	class, err := NewClass("testdata/references/local/simple.yaml", codec.NewYamlCodec(), data.NewPathFromOsPath("simple"))
 	assert.NoError(t, err)
 
-	references, err := ParseReferences(class)
-	assert.NoError(t, err)
-	for _, reference := range references {
-		assert.Contains(t, expectedReferencesSimple, reference)
-	}
+	testParse(t, class, expectedReferencesSimple)
 }
 
 func TestResolveReferencesSimple(t *testing.T) {
@@ -121,13 +121,7 @@ var expectedReferencesNested = []Reference{
 func TestParseReferencesNested(t *testing.T) {
 	class, err := NewClass("testdata/references/local/nested.yaml", codec.NewYamlCodec(), data.NewPathFromOsPath("nested"))
 	assert.NoError(t, err)
-
-	references, err := ParseReferences(class)
-	assert.NoError(t, err)
-	assert.NotNil(t, references)
-	for _, reference := range references {
-		assert.Contains(t, expectedReferencesNested, reference)
-	}
+	testParse(t, class, expectedReferencesNested)
 }
 
 func TestResolveReferencesNested(t *testing.T) {
@@ -250,13 +244,7 @@ var expectedReferencesChained = []Reference{
 func TestParseReferencesChained(t *testing.T) {
 	class, err := NewClass("testdata/references/local/chained.yaml", codec.NewYamlCodec(), data.NewPathFromOsPath("chained"))
 	assert.NoError(t, err)
-
-	references, err := ParseReferences(class)
-	assert.NoError(t, err)
-	assert.NotNil(t, references)
-	for _, reference := range references {
-		assert.Contains(t, expectedReferencesChained, reference)
-	}
+	testParse(t, class, expectedReferencesChained)
 }
 
 func TestResolveReferencesChained(t *testing.T) {
@@ -326,12 +314,7 @@ func TestParseReferencesCycle(t *testing.T) {
 		},
 	}
 
-	references, err := ParseReferences(class)
-	assert.NoError(t, err)
-	assert.NotNil(t, references)
-	for _, reference := range references {
-		assert.Contains(t, expected, reference)
-	}
+	testParse(t, class, expected)
 }
 
 func TestResolveReferencesCycle(t *testing.T) {
@@ -373,13 +356,7 @@ var expectedReferencesMulti = []Reference{
 func TestParseReferencesMulti(t *testing.T) {
 	class, err := NewClass("testdata/references/local/multi.yaml", codec.NewYamlCodec(), data.NewPathFromOsPath("multi"))
 	assert.NoError(t, err)
-
-	references, err := ParseReferences(class)
-	assert.NoError(t, err)
-	assert.NotNil(t, references)
-	for _, reference := range references {
-		assert.Contains(t, expectedReferencesMulti, reference)
-	}
+	testParse(t, class, expectedReferencesMulti)
 }
 
 func TestResolveReferencesMulti(t *testing.T) {
@@ -433,4 +410,261 @@ func TestReplaceReferencesMulti(t *testing.T) {
 	parsedReferences, err := ParseReferences(class)
 	assert.NoError(t, err)
 	assert.Len(t, parsedReferences, 0)
+}
+
+// --------------------------------------------------
+// Registry Reference Tests
+// --------------------------------------------------
+
+var expectedReferencesRegistrySimple = []Reference{
+	{
+		Path:       data.NewPath("test.name"),
+		TargetPath: data.NewPath("person.first_name"),
+	},
+	{
+		Path:       data.NewPath("test.name"),
+		TargetPath: data.NewPath("person.last_name"),
+	},
+	{
+		Path:       data.NewPath("person.age"),
+		TargetPath: data.NewPath("test.age"),
+	},
+}
+
+func TestParseReferencesRegistrySimple(t *testing.T) {
+	rootPath := "testdata/references/registry/simple"
+	registry := testCreateRegistry(t, rootPath, 2)
+	testParse(t, registry, expectedReferencesRegistrySimple)
+}
+
+func TestResolveReferencesRegistrySimple(t *testing.T) {
+	rootPath := "testdata/references/registry/simple"
+	registry := testCreateRegistry(t, rootPath, 2)
+	parsed := testParse(t, registry, expectedReferencesRegistrySimple)
+
+	resolved, err := ResolveReferences(parsed, registry)
+	assert.NoError(t, err)
+	assert.NotNil(t, resolved)
+
+	// This test does not use references with dependencies, hence the order doesn't matter
+	for _, res := range resolved {
+		assert.Contains(t, expectedReferencesRegistrySimple, res)
+	}
+}
+
+func TestReplaceReferencesRegistrySimple(t *testing.T) {
+	rootPath := "testdata/references/registry/simple"
+	registry := testCreateRegistry(t, rootPath, 2)
+
+	err := ReplaceReferences(expectedReferencesRegistrySimple, registry)
+	assert.NoError(t, err)
+
+	expected := map[string]data.Value{
+		"person.age": data.NewValue(30),
+		"test.name":  data.NewValue("John Doe"),
+	}
+
+	for path, expectedValue := range expected {
+		val, err := registry.GetPath(data.NewPath(path))
+		assert.NoError(t, err)
+		assert.Equal(t, expectedValue, val)
+	}
+}
+
+var expectedReferencesRegistryNested = []Reference{
+	{
+		Path:       data.NewPath("test.target"),
+		TargetPath: data.NewPath("nested.source"),
+	},
+	{
+		Path:       data.NewPath("test.target_array"),
+		TargetPath: data.NewPath("nested.source_array"),
+	},
+	{
+		Path:       data.NewPath("test.target_nested_map"),
+		TargetPath: data.NewPath("nested.nested_map"),
+	},
+	{
+		Path:       data.NewPath("test.target_nested_mixed"),
+		TargetPath: data.NewPath("nested.nested_mixed"),
+	},
+}
+
+func TestParseReferencesRegistryNested(t *testing.T) {
+	rootPath := "testdata/references/registry/nested"
+	registry := testCreateRegistry(t, rootPath, 2)
+	testParse(t, registry, expectedReferencesRegistryNested)
+}
+
+func TestResolveReferencesRegistryNested(t *testing.T) {
+	rootPath := "testdata/references/registry/nested"
+	registry := testCreateRegistry(t, rootPath, 2)
+	parsed := testParse(t, registry, expectedReferencesRegistryNested)
+
+	resolved, err := ResolveReferences(parsed, registry)
+	assert.NoError(t, err)
+	assert.NotNil(t, resolved)
+
+	// This test does not use references with dependencies, hence the order doesn't matter
+	for _, res := range resolved {
+		assert.Contains(t, expectedReferencesRegistryNested, res)
+	}
+
+	// Test: Attempt to resolve reference which is not valid within the class
+	invalidReferences := []Reference{
+		{
+			Path:       data.NewPath("test.target"),
+			TargetPath: data.NewPath("source"), // source path is not valid, should be 'nested.source'
+		},
+	}
+	resolved, err = ResolveReferences(invalidReferences, registry)
+	assert.ErrorIs(t, err, ErrUndefinedReferenceTarget)
+	assert.Nil(t, resolved)
+}
+
+func TestReplaceReferencesRegistryNested(t *testing.T) {
+	rootPath := "testdata/references/registry/nested"
+	registry := testCreateRegistry(t, rootPath, 2)
+
+	err := ReplaceReferences(expectedReferencesRegistryNested, registry)
+	assert.NoError(t, err)
+
+	expected := map[string]data.Value{
+		"test.target": data.NewValue(map[string]interface{}{
+			"foo": "bar",
+			"bar": "baz",
+		},
+		),
+		"test.target_array": data.NewValue([]interface{}{
+			"foo", "bar", "baz",
+		}),
+		"test.target_nested_map": data.NewValue(map[string]interface{}{
+			"foo": map[string]interface{}{
+				"bar": map[string]interface{}{
+					"baz": "qux",
+				},
+			},
+		}),
+		"test.target_nested_mixed": data.NewValue(map[string]interface{}{
+			"foo": []interface{}{
+				map[string]interface{}{
+					"bar": "baz",
+				},
+				"test",
+				map[string]interface{}{
+					"foo": map[string]interface{}{
+						"bar": "baz",
+					},
+				},
+				map[string]interface{}{
+					"array": []interface{}{
+						"one", "two", "three",
+					},
+				},
+			},
+		}),
+	}
+
+	for path, expectedValue := range expected {
+		val, err := registry.GetPath(data.NewPath(path))
+		assert.NoError(t, err)
+		assert.Equal(t, expectedValue, val)
+	}
+
+	// there must be no more references after replacing
+	parsedReferences, err := ParseReferences(registry)
+	assert.NoError(t, err)
+	assert.Len(t, parsedReferences, 0)
+}
+
+var expectedReferencesRegistryChained = []Reference{
+	{
+		Path:       data.NewPath("test.full_name"),
+		TargetPath: data.NewPath("person.first_name"),
+	},
+	{
+		Path:       data.NewPath("test.full_name"),
+		TargetPath: data.NewPath("person.last_name"),
+	},
+	{
+		Path:       data.NewPath("test.greet_person"),
+		TargetPath: data.NewPath("greeting.text"),
+	},
+	{
+		Path:       data.NewPath("test.greet_person"),
+		TargetPath: data.NewPath("full_name"),
+	},
+}
+
+func TestParseReferencesRegistryChained(t *testing.T) {
+	rootPath := "testdata/references/registry/chained"
+	registry := testCreateRegistry(t, rootPath, 2)
+	testParse(t, registry, expectedReferencesRegistryChained)
+}
+
+func TestResolveReferencesRegistryChained(t *testing.T) {
+	rootPath := "testdata/references/registry/chained"
+	registry := testCreateRegistry(t, rootPath, 2)
+	parsed := testParse(t, registry, expectedReferencesRegistryChained)
+
+	resolved, err := ResolveReferences(parsed, registry)
+	assert.NoError(t, err)
+	assert.NotNil(t, resolved)
+}
+
+func TestReplaceReferencesRegistryChained(t *testing.T) {
+	rootPath := "testdata/references/registry/chained"
+	registry := testCreateRegistry(t, rootPath, 2)
+
+	err := ReplaceReferences(expectedReferencesRegistryChained, registry)
+	assert.NoError(t, err)
+
+	expected := map[string]data.Value{
+		"test.full_name":    data.NewValue("John Doe"),
+		"test.greet_person": data.NewValue("Hello, John Doe"),
+	}
+
+	for path, expectedValue := range expected {
+		val, err := registry.GetPath(data.NewPath(path))
+		assert.NoError(t, err)
+		assert.Equal(t, expectedValue, val)
+	}
+
+	// there must be no more references after replacing
+	parsedReferences, err := ParseReferences(registry)
+	assert.NoError(t, err)
+	assert.Len(t, parsedReferences, 0)
+}
+
+// --------------------------------------------------
+// Test Helpers
+// --------------------------------------------------
+
+// testCreateRegistry quickly creates a registry with files from the given rootPath
+// and ensures that the registry is properly created.
+func testCreateRegistry(t *testing.T, rootPath string, expectedClassFileCound int) *Registry {
+	classFiles, err := DiscoverFiles(rootPath, codec.YamlPathSelector)
+	assert.NoError(t, err)
+	assert.Len(t, classFiles, expectedClassFileCound)
+
+	registry, err := NewRegistryFromFiles(classFiles, func(filePaths []string) ([]*Class, error) {
+		return ClassLoader(rootPath, classFiles, codec.NewYamlCodec())
+	})
+	assert.NoError(t, err)
+	assert.NotNil(t, registry)
+
+	return registry
+}
+
+// testParse is a helper to quickly test the 'ParseReferences' function
+// which assumes that the function does not return an error.
+// It checks that all passed references are returned.
+func testParse(t *testing.T, source ReferenceSourceWalker, expected []Reference) []Reference {
+	references, err := ParseReferences(source)
+	assert.NoError(t, err)
+	assert.NotNil(t, references)
+	for _, reference := range references {
+		assert.Contains(t, expected, reference)
+	}
+	return references
 }
